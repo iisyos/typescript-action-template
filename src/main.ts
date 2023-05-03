f8b16b812847ea241d2074796bf2e973a995fce6
iisyos <kosuke1216a@yahoo.co.jp>
2023-05-03 15:18:21 +0900

    setting

--- src/main.ts
+++ src/main.ts
@@ -20,18 +20,27 @@ async function run(): Promise<void> {
     if (github.context.payload.sender === undefined)
       throw new Error('sender is undefined')
     const senderId = github.context.payload.sender.login
+    // const senderId = "iisyos"
     const client = new ApolloClient({
       cache: new InMemoryCache(),
       uri: 'https://api.github.com/graphql',
       headers: {
-        authorization: `Bearer ${core.getInput('GITHUB_TOKEN')}`
+        authorization: `Bearer ${core.getInput('GRAPHQL_TOKEN')}`
+        // authorization: `Bearer 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`
       }
     })
     const {data} = await client.query({
       query: GET_USER_PROFILE,
-      variables: {senderId}
+      variables: {username: senderId}
     })
+    if (data === undefined || data.user === undefined)
+      throw new Error('user is undefined')
     core.debug(`The user profile: ${JSON.stringify(data, undefined, 2)}`)
+    if (data === undefined) throw new Error('user is undefined')
+    const twitterUsername = data.user.twitterUsername
+    if (twitterUsername === undefined)
+      throw new Error('twitterUsername is undefined')
+    core.setOutput('twitter_username', twitterUsername)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
