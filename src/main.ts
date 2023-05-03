06540f85a270736176b128fde6ac129e8d284449
iisyos <kosuke1216a@yahoo.co.jp>
2023-05-03 15:44:31 +0900

    setting

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {ApolloClient, gql, InMemoryCache} from '@apollo/client/core'
-import 'cross-fetch/polyfill'
-
-const GET_USER_PROFILE = gql`
-  query GetUserProfile($username: String!) {
-    user(login: $username) {
-      name
-      login
-      twitterUsername
-    }
-  }
-`
+import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
@@ -21,30 +10,25 @@ async function run(): Promise<void> {
     core.debug('1')
     const token = core.getInput('github-token')
     core.debug(token)
+    const response = (await getUserProfile(username)) as string
 
-    const client = new ApolloClient({
-      cache: new InMemoryCache(),
-      uri: 'https://api.github.com/graphql',
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    })
-    core.debug('2')
-    const {data} = await client.query({
-      query: GET_USER_PROFILE,
-      variables: {username}
-    })
-    core.debug('3')
-    if (data === undefined || data.user === undefined)
-      throw new Error('user is undefined')
-    core.debug(`The user profile: ${JSON.stringify(data, undefined, 2)}`)
-    if (data === undefined) throw new Error('user is undefined')
-    const twitterUsername = data.user.twitterUsername
-    if (twitterUsername === undefined)
-      throw new Error('twitterUsername is undefined')
-    core.setOutput('twitter_username', twitterUsername)
+    core.debug(response)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+async function getUserProfile(username: string): Promise<unknown> {
+  const url = `https://api.github.com/users/${username}`
+  const response = await fetch(url)
+  const data = await response.json()
+
+  if (response.ok) {
+    return data
+  } else {
+    throw new Error(
+      `Error fetching profile: ${JSON.stringify(data, undefined, 2)}`
+    )
   }
 }
 
