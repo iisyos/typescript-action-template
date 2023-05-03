65fc080c77ae72ce1c62e4ec285819f61994c226
iisyos <kosuke1216a@yahoo.co.jp>
2023-05-03 20:09:51 +0900

    setting

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,38 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import fetch from 'node-fetch'
-import {components} from '@octokit/openapi-types'
-
-type GitHubUser = components['schemas']['public-user']
+import {getUserProfile} from './profile'
+import {tweet} from './tweet'
+import {getAuthHeader} from './auth'
 
 async function run(): Promise<void> {
   try {
     if (github.context.payload.sender === undefined)
       throw new Error('sender is undefined')
     const username = github.context.payload.sender.login
-    core.debug('1')
-    const token = core.getInput('github-token')
-    core.debug(token)
     const response = await getUserProfile(username)
-    if (response.twitter_username === null) {
+    const twitter_username = response.twitter_username
+    if (twitter_username === null) {
       core.info('twitter_username is null')
       return
     }
-    core.info(`twitter_username is ${response.twitter_username}`)
+    core.info(`twitter_username is ${twitter_username}`)
+
+    const text = `@${twitter_username}\n${core.getInput('text')}`
+    await tweet(
+      text,
+      getAuthHeader(
+        {
+          key: core.getInput('consumer_key'),
+          secret: core.getInput('consumer_secret')
+        },
+        {
+          key: core.getInput('access_token'),
+          secret: core.getInput('access_token_secret')
+        }
+      )
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
-  }
-}
-
-async function getUserProfile(username: string): Promise<GitHubUser> {
-  const url = `https://api.github.com/users/${username}`
-  const response = await fetch(url)
-
-  if (response.ok) {
-    const data = (await response.json()) as GitHubUser
-    return data
-  } else {
-    throw new Error(`Error fetching profile: ${await response.json()}`)
   }
 }
 
