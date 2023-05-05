b2432e603c6b01e0a8e8ad9c94990fe3d9649cc9
iisyos <kosuke1216a@yahoo.co.jp>
2023-05-05 12:22:24 +0900

    aws-sdk

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);  
+    core.info(`The event payload: ${payload}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
